159d0c9f422b68f2e81ab6a04aa94928406a9d36
austin-schaefers <austinschaefers@Austins-MacBook-Pro.local>
2017-11-30 21:30:33 -0500

    update

--- Desktop/djangodev/426final/book/bookExchange/home/views.py
+++ Desktop/djangodev/426final/book/bookExchange/home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from . import forms
 
 # Create your views here.
 def home(request):
     myDict = {"insert_me": "text"}
     return render(request,"home/home.html", myDict)
+
+def ISBNfinder(request):
+    form = forms.homeForm
+    return render(request, "home/home.html", form)
 
