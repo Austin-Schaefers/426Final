c62257ee16e5703c17c6926d9a2c9db47f5c48c5
Adam Fish <adamleighfish@gmail.com>
2017-12-02 14:11:57 -0500

    Removed ManyToManyRelation

--- Desktop/djangodev/426final/book/bookExchange/home/models.py
+++ Desktop/djangodev/426final/book/bookExchange/home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 """ Student Table
 
 Standard db representation of a student
+Keeps track of all the books the students wants and if giving
 """
 class Student(models.Model):
     fname  = models.CharField("first name", max_length=20, unique=False, default="John")
@@ -31,8 +32,8 @@ class Listing(models.Model):
         ('W', 'Want'),
         ('G', 'Give'),
     )
-    sid  = models.ForeignKey(Student)
-    bid  = models.ForeignKey(Book)
+    sid  = models.ForeignKey(Student, on_delete=models.CASCADE)
+    bid  = models.ForeignKey(Book, on_delete=models.CASCADE)
     type = models.CharField(max_length=1, choices=TYPES)
     date = models.DateField(auto_now_add=True)
     open = models.BooleanField(default=True)
@@ -44,8 +45,8 @@ An exchange of books between two students.
 Field bid1 represents the book sid1 is giving away and vice versa with bid2/sid2
 """
 class Exchange(models.Model):
-    sid1 = models.ForeignKey(Student, related_name='+')
-    bid1 = models.ForeignKey(Book, related_name='+')
-    sid2 = models.ForeignKey(Student, related_name='+')
-    bid2 = models.ForeignKey(Book, related_name='+')
+    sid1 = models.ForeignKey(Student, related_name='+', on_delete=models.CASCADE)
+    bid1 = models.ForeignKey(Book, related_name='+', on_delete=models.CASCADE)
+    sid2 = models.ForeignKey(Student, related_name='+', on_delete=models.CASCADE)
+    bid2 = models.ForeignKey(Book, related_name='+', on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
