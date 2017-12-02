59bd14e29496459959814c73493b52e669a3579f
Adam Fish <adamleighfish@gmail.com>
2017-12-01 19:13:29 -0500

    Updated DB schema and registered models

--- Desktop/djangodev/426final/book/bookExchange/home/admin.py
+++ Desktop/djangodev/426final/book/bookExchange/home/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+import models
 
 # Register your models here.
 #remember to make superuser for everybody in the group pass: pass user: name
+admin.site.register(models.Student)
+admin.site.register(models.Book)
+admin.site.register(models.Listing)
+admin.site.register(models.Exchange)
--- Desktop/djangodev/426final/book/bookExchange/home/models.py
+++ Desktop/djangodev/426final/book/bookExchange/home/models.py
@@ -1,23 +1,52 @@
 from django.db import models
 
+""" Student Table
 
-# Create your models here.
-
+Standard db representation of a student
+"""
 class Student(models.Model):
-    firstName = models.CharField(max_length=12, unique=False)
-    lastName = models.CharField(max_length=12, unique=False)
-    password = models.CharField(max_length=12, unique=False)
-    email = models.EmailField(default="")
-    school = models.CharField(max_length=20, unique=False)
+    sid    = models.IntegerField(max_length=12, unique=True, primary_key=True)
+    fname  = models.CharField(max_length=20, unique=False)
+    lname  = models.CharField(max_length=20, unique=False)
+    pword  = models.CharField(max_length=40, unique=False)
+    email  = models.EmailField(default="")
+    school = models.CharField(max_length=40, unique=False)
 
+""" Book Table
 
+Standard db representation of a book
+"""
 class Book(models.Model):
-    sid = models.ForeignKey(Student)
-    ISBN = models.IntegerField(max_length=13)
-    title = models.CharField(max_length=25)
-    edition = models.IntegerField()
+    bid   = models.IntegerField(max_length=12, unique=True, primary_key=True)
+    isbn  = models.IntegerField(max_length=13)
+    title = models.CharField(max_length=40)
 
+""" Listing Table
+
+A listing of a book by a student.
+Field type indicates whether is book the student is listing is one he/she wants(W) or is giving(G).
+"""
+class Listing(models.Model):
+    TYPES = (
+        ('W', 'Want'),
+        ('G', 'Give'),
+    )
+    lid  = models.IntegerField(max_length=12, unique=True, primary_key=True)
+    sid  = models.ForeignKey(Student)
+    bid  = models.ForeignKey(Book)
+    type = models.CharField(max_length=1, choices=TYPES)
+    date = models.DateField(auto_now_add=True)
+    open = models.BooleanField(default=True)
+
+""" Exchange Table
+
+An exchange of books between two students.
+Field bid1 represents the book sid1 is giving away and vice versa with bid2/sid2
+"""
 class Exchange(models.Model):
-    eid = models.IntegerField(max_length=12, unique=True)
-
-
+    eid  = models.IntegerField(max_length=12, unique=True, primary_key=True)
+    sid1 = models.ForeignKey(Student)
+    bid1 = models.ForeignKey(Book)
+    sid2 = models.ForeignKey(Student)
+    bid2 = models.ForeignKey(Book)
+    date = models.DateField(auto_now_add=True)
